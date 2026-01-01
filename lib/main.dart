abd371f3ebf28df2e922fc35e3ab8e809c0e6716
o@neepo.dev <o@neepo.dev>
2026-01-01 23:43:41 +0300

    Use Auth services, add drawer, output logged in user data or login button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vkid_flutter_sdk/library_vkid.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_vk_id_auth_demo/services/auth/cubit/auth_cubit.dart';
+import 'package:flutter_vk_id_auth_demo/services/auth/storage/auth_storage.dart';
+import 'package:flutter_vk_id_auth_demo/services/auth/extensions/auth_context_x.dart';
+import 'package:flutter_vk_id_auth_demo/services/auth/repository/auth_repository.dart';
+import 'package:flutter_vk_id_auth_demo/ui/drawer/app_drawer.dart';
+import 'package:flutter_vk_id_auth_demo/ui/auth/login_bottom_sheet.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = AuthStorage(const FlutterSecureStorage());
+  final repository = AuthRepository(storage);
+
+  runApp(
+    BlocProvider(
+      create: (_) => AuthCubit(repository)..init(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,14 +40,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,8 +50,49 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Flutter VK ID Auth Demo'),
       ),
-      body: Center(
-        child: const Text('Hello world!'),
+      drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+
+              if (context.userIsLoggedIn) {
+                final User user = context.currentUser!;
+                return Column(
+                  spacing: 8,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(user.firstName),
+                    Text(user.lastName),
+                    Text(user.email),
+                  ],
+                );
+              }
+
+              return Column(
+                spacing: 10,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('User not logged in'),
+                  FilledButton.icon(
+                    icon: const Icon(Icons.login),
+                    label: const Text('Login'),
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        useSafeArea: true,
+                        builder: (_) => const LoginBottomSheet(),
+                      );
+                    },
+                  )
+                ],
+              );
+
+            },
+          ),
+        ),
       ),
     );
   }
