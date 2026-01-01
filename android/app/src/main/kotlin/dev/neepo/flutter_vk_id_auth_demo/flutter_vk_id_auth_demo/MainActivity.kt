1f6f145f5ade598ec950fc22bdb54862b2a3b629
o@neepo.dev <o@neepo.dev>
2026-01-01 22:14:41 +0300

    OneTap using crash fix: change FlutterActivity to FlutterFragmentActivity.

--- android/app/src/main/kotlin/dev/neepo/flutter_vk_id_auth_demo/flutter_vk_id_auth_demo/MainActivity.kt
+++ android/app/src/main/kotlin/dev/neepo/flutter_vk_id_auth_demo/flutter_vk_id_auth_demo/MainActivity.kt
@@ -1,5 +1,5 @@
 package dev.neepo.flutter_vk_id_auth_demo.flutter_vk_id_auth_demo
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
